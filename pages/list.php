81894e0f0cc4e1d3824e1084f36031e07abbc914
Wow-T3rminat0r <ujjalkatwal@gmail.com>
2021-01-26 09:42:15 +0000

    20210126　先生の答えからコピーぺした第一回小テスト

--- pages/list.php
+++ pages/list.php
@@ -1,3 +1,27 @@
+<?php
+/** リクエストパラメータを取得 */
+isset($_POST["area"]) ? $area = $_POST["area"] : $area = "";
+//var_dump($area);	// 作業-4
+/** データベース接続情報を設定 */
+$dsn = "mysql:host=localhost;dbname=restaurantdb;charset=utf8";
+$user = "restaurantdb_admin";
+$password = "admin123";
+
+// データベース接続オブジェクトを取得
+$pdo = new PDO($dsn, $user, $password);
+// 実行するSQLを設定
+$sql = "select * from restaurants where area = ?";
+// SQL実行オブジェクトを取得
+$pstmt = $pdo->prepare($sql);
+// プレースホルダにリクエストパラメータを設定
+$pstmt->bindValue(1, $area);
+// SQLを実行
+$pstmt->execute();
+// SQL実行結果を配列に取得
+$records = $pstmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($records); // 作業-5
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -26,81 +50,25 @@
 				</form>
 			</section>
 			</div>
+			<?php if (count($records) > 0): ?>
 			<section class="result">
-				<p>7件のレストランが見つかりました。</p>
+				<p><?= count($records) ?>件のレストランが見つかりました。</p>
 				<table class="list">
+					<?php foreach ($records as $record): ?>
 					<tr>
-						<td class="photo"><img name="image" alt="「Wine Bar ENOTECA」の写真" src="../pages/img/restaurant_1.jpg" /></td>
+						<td class="photo"><img name="image" alt="「<?= $record["name"] ?>」の写真" src="../pages/img/<?= $record["image"] ?>" /></td>
 						<td class="info">
 							<dl>
-								<dt name="name">Wine Bar ENOTECA</dt>
-								<dd name="description">常時10種類以上の赤・白ワインをご用意しています。<br />記念日にご来店ください。</dd>
+								<dt name="name"><?= $record["name"] ?></dt>
+								<dd name="description"><?= $record["description"] ?></dd>
 							</dl>
 						</td>
-						<td class="link"><a href="detail.html?id=1">詳細</a></td>
+						<td class="link"><a href="detail.php?id=<?= $record["id"] ?>">詳細</a></td>
 					</tr>
-					<tr>
-						<td class="photo"><img name="image" alt="「スペイン料理 ポルファボール！」の写真" src="../pages/img/restaurant_2.jpg" /></td>
-						<td class="info">
-							<dl>
-								<dt name="name">スペイン料理 ポルファボール！</dt>
-								<dd name="description">味が自慢。スペイン現地で学んだシェフが出す味は本物です。</dd>
-							</dl>
-						</td>
-						<td class="link"><a href="detail.html?id=2">詳細</a></td>
-					</tr>
-					<tr>
-						<td class="photo"><img name="image" alt="「パス・パスタ」の写真" src="../pages/img/restaurant_3.jpg" /></td>
-						<td class="info">
-							<dl>
-								<dt name="name">パス・パスタ</dt>
-								<dd name="description">本当のパスタを味わうならパス・パスタで！<br />休日の優雅なランチタイムに是非どうぞ。</dd>
-							</dl>
-						</td>
-						<td class="link"><a href="detail.php?id=3">詳細</a></td>
-					</tr>
-					<tr>
-						<td class="photo"><img name="image" alt="「レストラン有閑」の写真" src="../pages/img/restaurant_4.jpg" /></td>
-						<td class="info">
-							<dl>
-								<dt name="name">レストラン「有閑」</dt>
-								<dd name="description">広い店内で、お昼の優雅なひと時を過ごしませんか？</dd>
-							</dl>
-						</td>
-						<td class="link"><a href="detail.html?id=4">詳細</a></td>
-					</tr>
-					<tr>
-						<td class="photo"><img name="image" alt="「ビストロ　ルーヴル」の写真" src="../pages/img/restaurant_5.jpg" /></td>
-						<td class="info">
-							<dl>
-								<dt name="name">ビストロ「ルーヴル」</dt>
-								<dd name="description">高層ビル42階のビストロで、県内が一望できる。恋人とのひと時をここで過ごしませんか。</dd>
-							</dl>
-						</td>
-						<td class="link"><a href="detail.php?id=5">詳細</a></td>
-					</tr>
-					<tr>
-						<td class="photo"><img name="image" alt="「海沿いのレストラン La Mer」の写真" src="../pages/img/restaurant_6.jpg" /></td>
-						<td class="info">
-							<dl>
-								<dt name="name">海沿いのレストラン La Mer</dt>
-								<dd name="description">海が見える、海沿いのレストランです。</dd>
-							</dl>
-						</td>
-						<td class="link"><a href="detail.php?id=6">詳細</a></td>
-					</tr>
-					<tr>
-						<td class="photo"><img name="image" alt="「レストラン さくら」の写真" src="../pages/img/restaurant_7.jpg" /></td>
-						<td class="info">
-							<dl>
-								<dt name="name">レストラン さくら</dt>
-								<dd name="description">四季折々の自然を楽しむ伊豆市に、ひっそりと佇む隠れ家レストラン。\n旅行でいらっしゃった方も、お近くの方も、お気軽にお立ち寄りください。</dd>
-							</dl>
-						</td>
-						<td class="link"><a href="detail.php?id=7">詳細</a></td>
-					</tr>
+					<?php endforeach; ?>
 				</table>
 			</section>
+			<?php endif; ?>
 		</article>
 	</main>
 	<footer>
